6ffc6c8f9dac3af93b4dd8c07eba76a6b4fbf12f
pko <pawelkowol@gmail.com>
2024-02-08 10:23:00 +0100

    add validation messages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,9 @@ import { AddInvoiceComponent } from './pages/add-invoice/add-invoice.component';
 import { InvoicePreviewComponent } from './pages/invoice-preview/invoice-preview.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/new-invoice', pathMatch: 'full'},
-  {path: 'new-invoice', component: AddInvoiceComponent},
+  {path: '', component: AddInvoiceComponent},
   {path: 'invoice-preview', component: InvoicePreviewComponent},
-  {path: '**', redirectTo: '/new-invoice', pathMatch: 'full'},
+  {path: '**', redirectTo: '', pathMatch: 'full'},
 
 ];
 
--- src/app/pages/add-invoice/add-invoice.component.ts
+++ src/app/pages/add-invoice/add-invoice.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { InvoiceService } from '../../invoice.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-invoice',
@@ -7,10 +9,13 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
   styleUrl: './add-invoice.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AddInvoiceComponent implements OnInit{
+export class AddInvoiceComponent implements OnInit {
   invoiceForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+              private invoiceService: InvoiceService,
+              private router: Router) {
+
     this.invoiceForm = this.formBuilder.group({
       items: this.formBuilder.array([])
     });
@@ -22,13 +27,19 @@ export class AddInvoiceComponent implements OnInit{
 
   onSubmit() {
     console.log(this.invoiceForm);
+    if (this.items.length === 0) {
+
+    } else if (this.invoiceForm.valid) {
+      this.invoiceService.addItems(this.items.getRawValue());
+      this.router.navigate(['/invoice-preview']);
+    }
   }
 
   addNewItem() {
     const item = this.formBuilder.group({
       name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]),
-      count: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(100)]),
-      price: new FormControl(null, [Validators.required, Validators.min(1), Validators.max(1_000_000)])
+      count: new FormControl(1, [Validators.required, Validators.min(1), Validators.max(100)]),
+      price: new FormControl(1, [Validators.required, Validators.min(1), Validators.max(1_000_000)])
     });
     this.items.push(item);
   }
@@ -39,5 +50,10 @@ export class AddInvoiceComponent implements OnInit{
 
   get items() {
     return this.invoiceForm.controls['items'] as FormArray;
+  }
+
+  hasError(idx: number, controlName: string, errorCode: string): boolean | undefined {
+    console.log(`hasError check for ${idx}`);
+    return this.items.controls[idx].get(controlName)?.hasError(errorCode);
   }
 }
